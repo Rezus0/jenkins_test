dcb611828fee0b54645d535daedb546cc44e0120
Rezus <github.rzs@yandex.ru>
2024-12-17 23:41:59 +0300

    test1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World! (Jenkins TEST)");
         System.out.println("Hello World! (Jenkins TEST1)");
+        System.out.println("Hello World! (Jenkins TEST2)");
     }
 }
